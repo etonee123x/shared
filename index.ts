19b80e83fe826b655972cb41d1ddf538b47c2e8a
Evgeniy Uvarov <etonee123x@gmail.com>
2024-02-03 18:48:56 +0400

    upd

--- index.ts
+++ index.ts
@@ -35,7 +35,7 @@ export class ItemBase {
 
   numberOfThisExt?: number;
 
-  constructor ({ name, url, src, birthtime, numberOfThisExt }: ItemBase) {
+  constructor({ name, url, src, birthtime, numberOfThisExt }: ItemBase) {
     this.name = name;
     this.url = url;
     this.src = src;
@@ -65,7 +65,10 @@ export interface ItemFile<Ext extends string = string> extends ItemBase {
 
 export class ItemFile<Ext extends string = string> extends ItemBase {
   type: ITEM_TYPE.FILE = ITEM_TYPE.FILE;
-  constructor (baseItem: Omit<ItemFile<Ext>, 'ext' | 'type'>, public ext: Ext) {
+  constructor(
+    baseItem: Omit<ItemFile<Ext>, 'ext' | 'type'>,
+    public ext: Ext,
+  ) {
     super(baseItem);
   }
 }
@@ -82,10 +85,7 @@ export interface ItemAudio extends ItemFile<EXT_AUDIO> {
 export class ItemAudio extends ItemFile<EXT_AUDIO> {
   metadata: Metadata;
 
-  constructor (
-    { ext, ...baseItem }: Omit<ItemAudio, 'metadata'>,
-    metadata: Metadata,
-  ) {
+  constructor({ ext, ...baseItem }: Omit<ItemAudio, 'metadata'>, metadata: Metadata) {
     super(baseItem, ext);
     this.metadata = metadata;
   }
@@ -100,7 +100,7 @@ export enum EXT_PICTURE {
 export interface ItemPicture extends ItemFile<EXT_PICTURE> {}
 
 export class ItemPicture extends ItemFile<EXT_PICTURE> {
-  constructor ({ ext, ...baseItem }: ItemPicture) {
+  constructor({ ext, ...baseItem }: ItemPicture) {
     super(baseItem, ext);
   }
 }
--- utils.ts
+++ utils.ts
@@ -42,33 +42,41 @@ interface CustomError {
 
 const CUSTOM_ERROR_UNKNOWN_STATUS_CODE = -1;
 
-export const createError = ({ data, statusCode }: Partial<Omit<CustomError, '__isCustomError'>>) =>
-  ({ data, statusCode: isNil(statusCode) ? CUSTOM_ERROR_UNKNOWN_STATUS_CODE : statusCode, __isCustomError: true });
-
-export const createErrorClient = (data: CustomError['data']) => createError({
+export const createError = ({ data, statusCode }: Partial<Omit<CustomError, '__isCustomError'>>) => ({
   data,
-  statusCode: 400,
+  statusCode: isNil(statusCode) ? CUSTOM_ERROR_UNKNOWN_STATUS_CODE : statusCode,
+  __isCustomError: true,
 });
 
-export const createErrorServer = (data: CustomError['data']) => createError({
-  data,
-  statusCode: 500,
-});
+export const createErrorClient = (data: CustomError['data']) =>
+  createError({
+    data,
+    statusCode: 400,
+  });
 
-export const isCustomError = (arg: unknown): arg is CustomError =>
-  isRealObject(arg) && arg.__isCustomError === true;
+export const createErrorServer = (data: CustomError['data']) =>
+  createError({
+    data,
+    statusCode: 500,
+  });
+
+export const isCustomError = (arg: unknown): arg is CustomError => isRealObject(arg) && arg.__isCustomError === true;
 
 export const isCustomErrorClient = (arg: CustomError) => arg.statusCode >= 400 && arg.statusCode < 500;
 export const isCustomErrorServer = (arg: CustomError) => arg.statusCode >= 500 && arg.statusCode < 600;
 export const isCustomErrorUnknown = (arg: CustomError) => arg.statusCode === CUSTOM_ERROR_UNKNOWN_STATUS_CODE;
 
-export const omit
-  = <T1 extends Record<string, unknown>, T2 extends keyof T1>(object: T1, keys: Array<T2>): Omit<T1, T2> =>
-    Object.fromEntries(Object.entries(object).filter(([key]) => !keys.includes(key as T2))) as Omit<T1, T2>;
+export const omit = <T1 extends Record<string, unknown>, T2 extends keyof T1>(
+  object: T1,
+  keys: Array<T2>,
+): Omit<T1, T2> =>
+  Object.fromEntries(Object.entries(object).filter(([key]) => !keys.includes(key as T2))) as Omit<T1, T2>;
 
-export const pick
-  = <T1 extends Record<string, unknown>, T2 extends keyof T1>(object: T1, keys: Array<T2>): Pick<T1, T2> =>
-    Object.fromEntries(Object.entries(object).filter(([key]) => keys.includes(key as T2))) as Pick<T1, T2>;
+export const pick = <T1 extends Record<string, unknown>, T2 extends keyof T1>(
+  object: T1,
+  keys: Array<T2>,
+): Pick<T1, T2> =>
+  Object.fromEntries(Object.entries(object).filter(([key]) => keys.includes(key as T2))) as Pick<T1, T2>;
 
 export const isExtAudio = (ext: string): ext is EXT_AUDIO => Object.values<string>(EXT_AUDIO).includes(ext);
 export const isExtPicture = (ext: string): ext is EXT_PICTURE => Object.values<string>(EXT_PICTURE).includes(ext);
@@ -78,4 +86,4 @@ export const isItemFile = (item: Item): item is ItemFile => item.type === ITEM_T
 export const isItemAudio = (item: Item): item is ItemAudio => isItemFile(item) && isExtAudio(item.ext);
 export const isItemPicture = (item: Item): item is ItemPicture => isItemFile(item) && isExtPicture(item.ext);
 
-export const wrapToArray = <T>(arg: T | Array<T>): Array<T> => isArray(arg) ? arg : [arg];
+export const wrapToArray = <T>(arg: T | Array<T>): Array<T> => (isArray(arg) ? arg : [arg]);
