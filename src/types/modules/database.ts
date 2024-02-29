0753ddb1520162f0fa618cf825f06987b3dfdc80
Evgeniy Uvarov <etonee123x@gmail.com>
2024-02-29 20:52:35 +0400

    + Meta

--- src/types/modules/database.ts
+++ src/types/modules/database.ts
@@ -15,3 +15,16 @@ export type ForPost<T extends WithDatabaseFields> = Omit<T, 'id' | 'createdAt' |
 export type ForPut<T extends WithDatabaseFields> = Omit<T, 'updatedAt'>;
 
 export type ForPatch<T extends WithDatabaseFields> = Partial<Omit<T, 'updatedAt'>> & WithId & WithCreatedAt;
+
+export interface PaginationMeta {
+  perPage: number;
+  page: number;
+}
+
+export interface WithIsEnd {
+  isEnd: boolean;
+}
+
+export interface WithMeta<Meta> {
+  meta: Meta;
+}
