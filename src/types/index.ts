dfb08078b01f987fd807bb85095a69e53ab0d902
Evgeniy Uvarov <etonee123x@gmail.com>
2024-03-03 23:19:58 +0400

    upd

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+import { isNotNil } from '../utils';
+
 export * from './modules';
 
 export type Id = (string | number) & { readonly Id: unique symbol };
@@ -10,7 +12,8 @@ export interface WithId {
 
 export const toId = (id: Omit<Id, 'Id'>): Id => id as Id;
 
-export const areIdsEqual = (id1: Id, id2: Id) => String(id1) === String(id2);
+export const areIdsEqual = (id1: Id | Nil, id2: Id | Nil) =>
+  isNotNil(id1) && isNotNil(id2) && String(id1) === String(id2);
 
 export type NotEmptyArray<T> = [T, ...Array<T>];
 
@@ -20,6 +23,10 @@ export type Falsy = false | Nil | '' | 0 | 0n;
 
 export type Primitive = Nil | boolean | number | bigint | string | symbol;
 
-export type FunctionType = (...args: Array<any>) => any;
+export type PromiseOrNot<T> = T | Promise<T>;
+
+export type FunctionType<Return = any> = (...args: Array<any>) => Return;
+
+export type FunctionCallback = FunctionType<PromiseOrNot<void>>;
 
 export type ObjectType = Array<unknown> | Record<string, unknown> | FunctionType;
