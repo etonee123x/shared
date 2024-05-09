a8a2306b21a6c3e5a70f1591e36111c0fa135e09
Evgeniy Uvarov <etonee123x@gmail.com>
2024-05-09 16:29:22 +0400

    +upd

--- src/types/modules/blog.ts
+++ src/types/modules/blog.ts
@@ -2,6 +2,7 @@ import { type WithDatabaseFields } from '../';
 
 export interface PostData {
   text: string;
+  files: Array<File>;
 }
 
 export interface Post extends PostData, WithDatabaseFields {}
