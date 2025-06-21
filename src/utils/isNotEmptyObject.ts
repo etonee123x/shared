7af37dc1e37bccc9b95748e3072c85dfc06b8907
etonee123x <etonee123x@gmail.com>
2025-06-21 08:38:47 +0400

    - isNotEmptyArray

--- src/utils/isNotEmptyObject.ts
+++ src/utils/isNotEmptyObject.ts
@@ -1,5 +1,4 @@
 import { isRealObject } from './isRealObject';
-import { isNotEmptyArray } from './isNotEmptyArray';
 
 export const isNotEmptyObject = <T>(argument: T | Record<string, unknown>): argument is Record<string, unknown> =>
-  isRealObject(argument) && isNotEmptyArray(Object.values(argument));
+  isRealObject(argument) && Boolean(Object.values(argument).length);
